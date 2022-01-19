2ef3b54f3d49bbc1a15d47a79ddb67dc49c913cf
naoto-naka <luckywhitecat770@gmail.com>
2022-01-19 21:33:05 +0900

    #17-Add-infra-task-test

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,35 @@
-FROM golang:1.17.5-alpine3.15
+FROM golang:1.17.5-alpine3.15 AS dev
 
-WORKDIR /go/src/github.com/selfManager
-
+WORKDIR /go/src
 RUN apk update &&\
     apk upgrade &&\
     apk add git
-
+COPY go.mod go.sum ./
+RUN go mod download
 RUN go install github.com/cosmtrek/air@latest
 COPY .air.toml .
 
 CMD ["air", "-c", ".air.toml"]
+
+
+
+FROM golang:1.17.5-alpine3.15 AS builder
+
+WORKDIR /go/src
+COPY . .
+RUN go mod download
+ARG CGO_ENABLED=0
+ARG GOOS=linux
+ARG GOARCH=amd64
+RUN go build \
+    -o /go/bin/main
+
+
+
+FROM scratch  AS prod
+WORKDIR /go/src
+COPY ./templates ./templates
+
+COPY --from=builder /go/bin/main /go/bin/main
+
+ENTRYPOINT [ "/go/bin/main" ]
---
+++ infrastructure/persistence/want_data.go
@@ -0,0 +1,35 @@
+package persistence
+
+import (
+	"selfManager/domain/model"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+var success = map[string]*[]model.Task{
+	"全件取得": {
+		{
+			Model: gorm.Model{
+				ID:        1,
+				CreatedAt: time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local),
+				UpdatedAt: time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local),
+				DeletedAt: *new(gorm.DeletedAt),
+			},
+			Name:        "test1",
+			Status:      1,
+			DueDatetime: time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local),
+		},
+		{
+			Model: gorm.Model{
+				ID:        2,
+				CreatedAt: time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local),
+				UpdatedAt: time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local),
+				DeletedAt: *new(gorm.DeletedAt),
+			},
+			Name:        "test2",
+			Status:      2,
+			DueDatetime: time.Date(2022, time.January, 1, 0, 0, 0, 0, time.Local),
+		},
+	},
+}
